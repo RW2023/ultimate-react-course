0525a4ccee3e2a24372906a8a5d6390a6824cd51
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-02 20:03:04 -0500

    Add daisyui package and update tailwind.config.js

--- challenge_one/tailwind.config.js
+++ challenge_one/tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: true,
   },
-  plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        overlay: '#f4fff8ff', // Mint Cream
+        background: '#000f08ff', // Night
+        button: '#F8F32B', // Cadet Gray
+        headline: '#f4fff8ff', // Mint Cream
+        paragraph: '#8baaadff', // Cadet Gray
+        buttonText: '#000f08ff', // Night
+        stroke: '#4d4847ff', // Davy's Gray
+        main: '#8baaadff', // Cadet Gray
+        highlight: '#1c3738ff', // Gunmetal
+        secondary: '#f4fff8ff', // Mint Cream
+        tertiary: '#4d4847ff', // Davy's Gray
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans'],
+        serif: ['Roboto Slab', 'serif'],
+      },
+    },
+  },
+  // eslint-disable-next-line no-undef
+  plugins: [require('daisyui')],
 };
