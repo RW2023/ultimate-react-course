18c75ac9094877a1afa82649d294582c6796e830
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-03 00:26:02 -0500

    Update styling and component  in App.jsx. adjust width on different devices

--- challenge_one/src/App.jsx
+++ challenge_one/src/App.jsx
@@ -1,34 +1,39 @@
 import './App.css';
 import img from '/me.jpg';
-import Heading from '../src/Components/Heading';
-import SubHeading from '../src/Components/SubHeading';
+import LightHeading from '../src/Components/Heading';
+import LightSubHeading from '../src/Components/SubHeading';
 import Intro from './Components/Intro';
+import SubHeading from './Components/SubHeading';
 
 function App() {
   const me = img;
 
 
   return (
-    <div className="flex justify-center items-center p-4 min-h-screen bg-base300 glass">
-      <div className="card card-compact w-96 bg-white shadow-xl border ">
+    <div className="flex justify-center items-center p-4 min-h-screen bg-base300 glass mx-auto">
+      <div className="card card-compact w-3/4 md:w-1/2 bg-base-300 shadow-2xl border mx-auto">
         <figure>
           <img
             src={me}
             alt="Ryan Wilson"
             height={400}
             width={400}
-            className="rounded-md h-50 w-50 mx-2 border-black border-2 justify-self-start  p1 bg-accent"
+            className="rounded-md h-50 w-50 mx-2 border-black border-2 justify-self-start  p-1 "
           />
         </figure>
         <div className="card-body">
-          <Heading title="Ryan Wilson" iconClass="fas fa-user-circle" />
-          <SubHeading title="web developer" />
+          <LightHeading title="Ryan Wilson" iconClass="fas fa-user-circle" />
+          <LightSubHeading title="web developer" iconClass='fas fa-code'/>
           <div className="card-content text-lg">
             <Intro />
           </div>
+          <div id='skills'>
+            <SubHeading title ="skills" iconClass="fas fa-laptop-code"/>
+
+          </div>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Action 1</button>
-            <button className="btn btn-secondary">Action 2</button>
+            <button className="btn btn-primary">Front End</button>
+            <button className="btn btn-secondary">Back End</button>
           </div>
         </div>
       </div>
