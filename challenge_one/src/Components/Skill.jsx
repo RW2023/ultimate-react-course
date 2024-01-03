a70292f27fc61b321d33246c5298ce2e66369739
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-03 02:50:18 -0500

    Add glass effect and border to Skill component

--- challenge_one/src/Components/Skill.jsx
+++ challenge_one/src/Components/Skill.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 const Skill = ({ skill, icon }) => {
     return (
-        <div className="flex items-center text-2xl">
+        <div className="flex items-center text-2xl glass border rounded drop-shadow-xl p-1 m-1">
             <i className={`fab fa-${icon} mr-2`}></i>
             <p>{skill}</p>
         </div>
