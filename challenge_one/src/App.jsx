77f9dad674050d1a038620e25487f12bcd2f7c55
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-02 23:29:06 -0500

    Update styling and image size in App component

--- challenge_one/src/App.jsx
+++ challenge_one/src/App.jsx
@@ -9,19 +9,22 @@ function App() {
 
 
   return (
-    <div className="flex justify-center items-center p-4 min-h-screen bg-background">
-      <div className="card card-compact w-96 bg-base-100 shadow-xl border">
+    <div className="flex justify-center items-center p-4 min-h-screen bg-base300 glass">
+      <div className="card card-compact w-96 bg-white shadow-xl border ">
         <figure>
-          <img src={me} alt="Ryan Wilson" />
+          <img
+            src={me}
+            alt="Ryan Wilson"
+            height={400}
+            width={400}
+            className="rounded-md h-50 w-50 mx-2 border-black border-2 justify-self-start  p1 bg-accent"
+          />
         </figure>
         <div className="card-body">
-         <Heading 
-         title="Ryan Wilson"
-          iconClass="fas fa-user-circle"
-         />
-         <SubHeading title="web developer" />
+          <Heading title="Ryan Wilson" iconClass="fas fa-user-circle" />
+          <SubHeading title="web developer" />
           <div className="card-content text-lg">
-           <Intro />
+            <Intro />
           </div>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Action 1</button>
