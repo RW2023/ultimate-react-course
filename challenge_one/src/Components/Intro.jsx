b023f408938d044a0cbf8abb700c9e481c10c83d
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-07 07:21:15 -0500

    first commit

--- challenge_one/src/Components/Intro.jsx
+++ challenge_one/src/Components/Intro.jsx
@@ -6,7 +6,7 @@ export default function Intro() {
   return (
     <div className="flex flex-col justify-center bg-background p-1 border rounded-md drop-shadow-xl">
       <SubHeading title="Hi my name is ryan wilson" iconClass="fas fa-hand-sparkles" />
-      <p>
+      <p className='text-lg'>
         I am a full stack web developer based in
         Ontario Canada with a passion for creating beautiful, performant and
         functional web applications. When I am not coding I enjoy spending time
